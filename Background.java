7c2d7d6130cab1764c7c94e6c4d2cda6d2ec6142
alexedwards <agedwards23@students.desu.edu>
2024-04-25 22:03:09 -0400

    blackjack

--- Background.java
+++ Background.java
@@ -9,11 +9,11 @@ public enum Background
     CHAD(0,"CHAD"),LIBYA (1,"LIBYA"), MALAWI(2,"MALAWI"), MALI(3,"MALI"),
     MADAGASCAR(4,"MADAGASCAR");
 
-    private int rank;
+    private int rnk;
     private String rep;
 
-    private Background(int rank, String representation){
-        this.rank = rank;
+    private Background(int rnk, String representation){
+        this.rnk = rnk;
         rep = representation;
     }
     
